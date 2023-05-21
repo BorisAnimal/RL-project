07577c033fc6b33da8a5db0abc0c6ac8959d885d
BorisAnimal <iguru98@yandex.ru>
2023-05-21 04:35:52 +0300

    final refactoring

--- test_model.py
+++ test_model.py
@@ -1,29 +1,35 @@
 """
 Test a trained SAC agent on the SimpleEnv environment
 """
+import sys
 
-from stable_baselines3 import SAC, PPO
-from stable_baselines3.common.vec_env import VecFrameStack, DummyVecEnv
+from stable_baselines3 import SAC
+from stable_baselines3.common.vec_env import DummyVecEnv
 
 from environmetns.env_dynamic import DynamicEnv
 from environmetns.env_maze import MazeEnv
 from environmetns.env_simple import SimpleEnv
 from environmetns.player import SimplePlayer
 
-# MODEL_PATH = "models/sac_simple_pretrained_model_20000000_steps.zip"
-# MODEL_PATH = "tmp/sac_dynamic_pretrained_model_1600000_steps.zip"
-MODEL_PATH = "models/sac_maze_pretrained_model_9760000_steps.zip"
-
-# Create and wrap the environment
-# environmetns = DummyVecEnv([lambda: SimpleEnv(player=SimplePlayer(), time_limit=30, render_mode='human')])
-env = DummyVecEnv([lambda: MazeEnv(player=SimplePlayer(), time_limit=30, render_mode='human')])
-# env = DummyVecEnv([lambda: DynamicEnv(player=SimplePlayer(), time_limit=30, render_mode='human')])
-
 if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        raise Exception("Expected environment name as argument. Exit..")
+    env_name = sys.argv[1]
+
+    if env_name == 'simple':
+        MODEL_PATH = "models/sac_simple_pretrained_model_20000000_steps.zip"
+        env = DummyVecEnv([lambda: SimpleEnv(player=SimplePlayer(), time_limit=30, render_mode='human')])
+    elif env_name == 'maze':
+        MODEL_PATH = "models/sac_maze_pretrained_model_9760000_steps.zip"
+        env = DummyVecEnv([lambda: MazeEnv(player=SimplePlayer(), time_limit=30, render_mode='human')])
+    elif env_name == 'dynamic':
+        MODEL_PATH = "models/sac_dynamic_pretrained_model_3200000_steps.zip"
+        env = DummyVecEnv([lambda: DynamicEnv(player=SimplePlayer(), time_limit=30, render_mode='human')])
+    else:
+        raise Exception(f'Unexpected environment with name {env_name}. Please, choose "simple", "maze" or "dynamic"')
     # Evaluate the agent
-    # Load the trained agent
     model = SAC.load(MODEL_PATH, env=env)
-    for i in range(10):
+    for i in range(300):
         obs = env.reset()
         done = False
         episode_reward = 0
@@ -35,4 +41,3 @@ if __name__ == '__main__':
             if done:
                 env.reset()
         print("Episode reward", episode_reward)
-        env.render("yes")
